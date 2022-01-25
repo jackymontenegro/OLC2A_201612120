b24867f29b5835549be0fe5786d0525e9fe9643d
jackymontenegro <3007065960101@ingenieria.usac.edu.gt>
2022-01-24 20:55:10 -0600

    [ADD] Ejercicio1

---
+++ Practica1/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+import java.io.FileInputStream;
+
+/**
+ *
+ * @author jacky
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        interpretar("entrada.txt");
+        
+    }
+   private static void interpretar(String path) {
+        fuentes.parser parse;
+        try {
+            parse = new fuentes.parser(new fuentes.scanner(new FileInputStream(path)));
+            parse.parse();
+        } catch(Exception e) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println("Causa: "+e.getCause());
+        }
+    }
+    
+}
---
+++ Practica1/Ejercicio1/src/fuentes/lexico.jflex
@@ -0,0 +1,39 @@
+package fuentes;
+
+import java_cup.runtime.Symbol;
+
+//Scanner para generar C3D
+
+%%
+%{
+    //Código de usuario
+%}
+
+%cup
+%class scanner
+%public
+%line
+%char
+%column
+%full
+
+
+
+
+PALABRA = [A-Za-z]+
+SPACE   = [\ \r\t\f\t]
+ENTER   = [\ \n]
+
+%%
+
+"," {return new Symbol(sym.COMA,yyline,yychar, yytext());} 
+
+<YYINITIAL> {PALABRA}       {return new Symbol(sym.PALABRA, yyline, yycolumn, yytext());}  
+
+<YYINITIAL> {SPACE}     { /*Espacios en blanco, ignorados*/ }
+<YYINITIAL> {ENTER}     { /*Saltos de linea, ignorados*/}
+
+<YYINITIAL> . {
+        String errLex = "Error léxico, caracter irreconocible: '"+yytext()+"' en la línea: "+(yyline+1)+" y columna: "+yycolumn;
+        System.err.println(errLex);
+}
